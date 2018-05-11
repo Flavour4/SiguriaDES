b463713bc27e84672fc5d439298a44a9cf67ba22
Melosid <35465480+Melosid@users.noreply.github.com>
2018-05-11 10:04:31 +0200

    DES
    DES

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.Ngarko = new System.Windows.Forms.Button();
+            this.txtCelesi = new System.Windows.Forms.TextBox();
+            this.btnEnkriptoDES = new System.Windows.Forms.Button();
+            this.btnDekripto = new System.Windows.Forms.Button();
+            this.btnEnkripto3DES = new System.Windows.Forms.Button();
+            this.btnDekripto3DES = new System.Windows.Forms.Button();
+            this.btnEkripto2DES = new System.Windows.Forms.Button();
+            this.btnDekripto2DES = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(116, 174);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(271, 20);
+            this.txtFile.TabIndex = 0;
+            // 
+            // Ngarko
+            // 
+            this.Ngarko.Location = new System.Drawing.Point(403, 174);
+            this.Ngarko.Name = "Ngarko";
+            this.Ngarko.Size = new System.Drawing.Size(76, 20);
+            this.Ngarko.TabIndex = 1;
+            this.Ngarko.Text = "...";
+            this.Ngarko.UseVisualStyleBackColor = true;
+            this.Ngarko.Click += new System.EventHandler(this.Ngarko_Click);
+            // 
+            // txtCelesi
+            // 
+            this.txtCelesi.Location = new System.Drawing.Point(116, 246);
+            this.txtCelesi.Name = "txtCelesi";
+            this.txtCelesi.Size = new System.Drawing.Size(271, 20);
+            this.txtCelesi.TabIndex = 2;
+            // 
+            // btnEnkriptoDES
+            // 
+            this.btnEnkriptoDES.Location = new System.Drawing.Point(64, 312);
+            this.btnEnkriptoDES.Name = "btnEnkriptoDES";
+            this.btnEnkriptoDES.Size = new System.Drawing.Size(76, 29);
+            this.btnEnkriptoDES.TabIndex = 3;
+            this.btnEnkriptoDES.Text = "EnkDes";
+            this.btnEnkriptoDES.UseVisualStyleBackColor = true;
+            this.btnEnkriptoDES.Click += new System.EventHandler(this.btnEnkriptoDES_Click);
+            // 
+            // btnDekripto
+            // 
+            this.btnDekripto.Location = new System.Drawing.Point(64, 362);
+            this.btnDekripto.Name = "btnDekripto";
+            this.btnDekripto.Size = new System.Drawing.Size(76, 29);
+            this.btnDekripto.TabIndex = 4;
+            this.btnDekripto.Text = "DekDes";
+            this.btnDekripto.UseVisualStyleBackColor = true;
+            this.btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
+            // 
+            // btnEnkripto3DES
+            // 
+            this.btnEnkripto3DES.Location = new System.Drawing.Point(390, 312);
+            this.btnEnkripto3DES.Name = "btnEnkripto3DES";
+            this.btnEnkripto3DES.Size = new System.Drawing.Size(76, 29);
+            this.btnEnkripto3DES.TabIndex = 5;
+            this.btnEnkripto3DES.Text = "Enk3Des";
+            this.btnEnkripto3DES.UseVisualStyleBackColor = true;
+            this.btnEnkripto3DES.Click += new System.EventHandler(this.btnEnkripto3DES_Click);
+            // 
+            // btnDekripto3DES
+            // 
+            this.btnDekripto3DES.Location = new System.Drawing.Point(390, 366);
+            this.btnDekripto3DES.Name = "btnDekripto3DES";
+            this.btnDekripto3DES.Size = new System.Drawing.Size(76, 29);
+            this.btnDekripto3DES.TabIndex = 6;
+            this.btnDekripto3DES.Text = "Dek3Des";
+            this.btnDekripto3DES.UseVisualStyleBackColor = true;
+            this.btnDekripto3DES.Click += new System.EventHandler(this.btnDekripto3DES_Click);
+            // 
+            // btnEkripto2DES
+            // 
+            this.btnEkripto2DES.Location = new System.Drawing.Point(219, 312);
+            this.btnEkripto2DES.Name = "btnEkripto2DES";
+            this.btnEkripto2DES.Size = new System.Drawing.Size(76, 29);
+            this.btnEkripto2DES.TabIndex = 7;
+            this.btnEkripto2DES.Text = "Enk2Des";
+            this.btnEkripto2DES.UseVisualStyleBackColor = true;
+            this.btnEkripto2DES.Click += new System.EventHandler(this.btnEkripto2DES_Click);
+            // 
+            // btnDekripto2DES
+            // 
+            this.btnDekripto2DES.Location = new System.Drawing.Point(219, 362);
+            this.btnDekripto2DES.Name = "btnDekripto2DES";
+            this.btnDekripto2DES.Size = new System.Drawing.Size(76, 29);
+            this.btnDekripto2DES.TabIndex = 8;
+            this.btnDekripto2DES.Text = "Dek2Des";
+            this.btnDekripto2DES.UseVisualStyleBackColor = true;
+            this.btnDekripto2DES.Click += new System.EventHandler(this.btnDekripto2DES_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Celesi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ngarko";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(333, 48);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Enkriptimi dhe dekriptimi i fajllit me\r\n              2DES dhe 3DES";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 407);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDekripto2DES);
+            this.Controls.Add(this.btnEkripto2DES);
+            this.Controls.Add(this.btnDekripto3DES);
+            this.Controls.Add(this.btnEnkripto3DES);
+            this.Controls.Add(this.btnDekripto);
+            this.Controls.Add(this.btnEnkriptoDES);
+            this.Controls.Add(this.txtCelesi);
+            this.Controls.Add(this.Ngarko);
+            this.Controls.Add(this.txtFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button Ngarko;
+        private System.Windows.Forms.TextBox txtCelesi;
+        private System.Windows.Forms.Button btnEnkriptoDES;
+        private System.Windows.Forms.Button btnDekripto;
+        private System.Windows.Forms.Button btnEnkripto3DES;
+        private System.Windows.Forms.Button btnDekripto3DES;
+        private System.Windows.Forms.Button btnEkripto2DES;
+        private System.Windows.Forms.Button btnDekripto2DES;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using static System.Windows.Forms.LinkLabel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+
+
+        private void EncryptFile(string source, string destination)
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            FileStream fsInput = new FileStream(source, FileMode.Open, FileAccess.Read);
+            FileStream fsEncrypted = new FileStream(destination, FileMode.Create, FileAccess.Write);
+
+            DES.Key = Encoding.UTF8.GetBytes(txtCelesi.Text);
+            DES.IV = Encoding.UTF8.GetBytes("11223344");
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            CryptoStream cryptostream = new CryptoStream(fsEncrypted,
+                                desencrypt,
+                                CryptoStreamMode.Write);
+            
+            byte[] bytearrayinput = new byte[fsInput.Length];
+            fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Close();
+            fsInput.Close();
+            fsEncrypted.Close();
+        }
+        private void DecryptFile(string source, string destination)
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+
+            DES.Key = Encoding.UTF8.GetBytes(txtCelesi.Text);
+            DES.IV = Encoding.UTF8.GetBytes("11223344");
+
+            FileStream fsread = new FileStream(source,
+                                           FileMode.Open,
+                                           FileAccess.Read);
+
+            ICryptoTransform desdecrypt = DES.CreateDecryptor();
+            FileStream fswrite = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            CryptoStream cryptostreamDecr = new CryptoStream(fswrite,
+                                                         desdecrypt,
+                                                         CryptoStreamMode.Write);
+            
+            byte[] byteinput = new byte[fsread.Length];
+            fsread.Read(byteinput, 0, byteinput.Length);
+
+            cryptostreamDecr.Write(byteinput, 0, byteinput.Length);
+
+            fsread.Close();
+            fswrite.Close();
+        }
+
+
+        string fajlli = "C:\\Users\\melos\\Desktop\\MyFile.txt";
+        private void EncryptFile2des(string source, string destination)
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            FileStream fsread = new FileStream(source, FileMode.Open, FileAccess.Read);
+            FileStream fswrite = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            FileStream fsfajlliwrite = new FileStream(fajlli, FileMode.Create, FileAccess.Write);
+
+
+
+            DES.Key = Encoding.UTF8.GetBytes(txtCelesi.Text);
+            DES.IV = Encoding.UTF8.GetBytes("11223344");
+            DES.Mode = CipherMode.CBC;
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            ICryptoTransform desdecrypt = DES.CreateDecryptor();
+
+            CryptoStream cryptostream = new CryptoStream(fsfajlliwrite,
+                                desencrypt,
+                                CryptoStreamMode.Write);
+            byte[] bytearrayinput = new byte[fsread.Length];
+
+            fsread.Read(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Close();
+            fsfajlliwrite.Close();
+            FileStream fsfajlliopen = new FileStream(fajlli, FileMode.Open, FileAccess.Read);
+
+
+            CryptoStream crptstream = new CryptoStream(fswrite, desencrypt, CryptoStreamMode.Write);
+
+            byte[] bytearrayinput1 = new byte[fsfajlliopen.Length];
+            fsfajlliopen.Read(bytearrayinput1, 0, bytearrayinput1.Length);
+            crptstream.Write(bytearrayinput1, 0, bytearrayinput1.Length);
+            crptstream.Close();
+            fsread.Close();
+            fsfajlliopen.Close();
+            fswrite.Close();
+
+        }
+        private void DecryptFile2des(string source, string destination)
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            FileStream fsread = new FileStream(source, FileMode.Open, FileAccess.Read);
+            FileStream fswrite = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            FileStream fsfajlliwrite = new FileStream(fajlli, FileMode.Create, FileAccess.Write);
+
+            DES.Key = Encoding.UTF8.GetBytes(txtCelesi.Text);
+            DES.IV = Encoding.UTF8.GetBytes("11223344");
+            ICryptoTransform desdecrypt = DES.CreateDecryptor();
+
+            CryptoStream cryptostream = new CryptoStream(fsfajlliwrite,
+                                desdecrypt,
+                                CryptoStreamMode.Write);
+            byte[] bytearrayinput = new byte[fsread.Length];
+
+            fsread.Read(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Close();
+            fsfajlliwrite.Close();
+            FileStream fsfajlliopen = new FileStream(fajlli, FileMode.Open, FileAccess.Read);
+            CryptoStream crptstream = new CryptoStream(fswrite, desdecrypt, CryptoStreamMode.Write);
+
+
+            byte[] bytearrayinput1 = new byte[fsfajlliopen.Length];
+            fsfajlliopen.Read(bytearrayinput1, 0, bytearrayinput1.Length);
+            crptstream.Write(bytearrayinput1, 0, bytearrayinput1.Length);
+
+            //cryptostream.Flush();
+            //crptstream.Flush();
+            cryptostream.Close();
+            crptstream.Close();
+            fsread.Close();
+            fswrite.Close();
+            fsfajlliopen.Close();
+        }
+
+
+
+
+        private void EncryptFile3des(string source, string destination)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+            DES.Key = Encoding.UTF8.GetBytes(txtCelesi.Text);
+            DES.IV = Encoding.UTF8.GetBytes("11223344");
+            // Create a FileStream object to read in the source file.
+            FileStream fsInput = new FileStream(source, FileMode.Open, FileAccess.Read);
+            // Create a byte array from the FileStream object by reading in the source file.
+
+            byte[] abytInput = new byte[fsInput.Length];
+            fsInput.Read(abytInput, 0, Convert.ToInt32(fsInput.Length));
+            
+
+            // Create a FileStream object to write to a temp file.
+
+            FileStream fsCipherText = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            fsCipherText.SetLength(0);
+
+            // Create a Crypto Stream that transforms the file stream using the chosen 
+            // encryption and writes it to the output FileStream object.
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            CryptoStream csEncrypted = new CryptoStream(fsCipherText, desencrypt,
+                CryptoStreamMode.Write);
+            csEncrypted.Write(abytInput, 0, abytInput.Length);
+            csEncrypted.Close();
+            fsInput.Close();
+            fsCipherText.Close();
+
+        }
+        private void DecryptFile3des(string source, string destination)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+
+            DES.Key = Encoding.UTF8.GetBytes(txtCelesi.Text);
+            DES.IV = Encoding.UTF8.GetBytes("11223344");
+
+            FileStream fsread = new FileStream(source,
+                                           FileMode.Open,
+                                           FileAccess.Read);
+
+            ICryptoTransform desdecrypt = DES.CreateDecryptor();
+            FileStream fswrite = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            CryptoStream cryptostreamDecr = new CryptoStream(fswrite,
+                                                         desdecrypt,
+                                                         CryptoStreamMode.Write);
+
+ 
+            byte[] byteinput = new byte[fsread.Length];
+            fsread.Read(byteinput, 0, byteinput.Length);
+            cryptostreamDecr.Write(byteinput, 0, byteinput.Length);
+            fsread.Close();
+            fswrite.Close();
+
+        }
+
+
+
+
+
+        private void Ngarko_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string source = ofd.FileName;
+                string path = source.ToString();
+                txtFile.Text = path;
+            }
+        }
+
+        private void btnEnkriptoDES_Click(object sender, EventArgs e)
+        {
+            string source = ofd.FileName;
+            saveFileDialog1.Filter = "Text Format(*.txt)| *.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string destination = saveFileDialog1.FileName;
+                EncryptFile(source, destination);
+            }
+        }
+
+        private void btnEnkripto3DES_Click(object sender, EventArgs e)
+        {
+            string source = ofd.FileName;
+            saveFileDialog1.Filter = "Text Format(*.txt)| *.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string destination = saveFileDialog1.FileName;
+                EncryptFile3des(source, destination);
+            }
+        }
+
+        private void btnDekripto_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Text Format(*.txt)| *.txt";
+
+                string source = ofd.FileName;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string destination = saveFileDialog1.FileName;
+                    DecryptFile(source, destination);
+                }
+
+            MessageBox.Show("U Dekriptua me sukses");
+        }
+
+
+        private void btnDekripto3DES_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Text Format(*.txt)| *.txt";
+                string source = ofd.FileName;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string destination = saveFileDialog1.FileName;
+                    DecryptFile3des(source, destination);
+                }
+            MessageBox.Show("U Dekriptua me sukses");
+        }
+
+
+        private void btnDekripto2DES_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Text Format(*.txt)| *.txt";
+            string source = ofd.FileName;
+            string fajlli = "C:\\Users\\melos\\Desktop\\MyFile.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string destination = saveFileDialog1.FileName;
+                DecryptFile2des(source, destination);
+            }
+
+            MessageBox.Show("U Dekriptua me sukses");
+        }
+
+        private void btnEkripto2DES_Click(object sender, EventArgs e)
+        {
+            string source = ofd.FileName;
+            string fajlli = "C:\\Users\\melos\\Desktop\\MyFile.txt";
+            saveFileDialog1.Filter = "Text Format(*.txt)| *.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string destination = saveFileDialog1.FileName;
+                EncryptFile2des(source, destination);
+            }
+        }
+    }
+}
